5c0791b8d19fca585d3d2b6b1b4e4b7c83c93f91
Sharmin2622 <islam.sharmin111@gmail.com>
2024-06-10 23:17:30 +0600

    vercel json file

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("quoraDb").collection("users");
     const postCollection = client.db("quoraDb").collection("posts");
@@ -280,10 +280,9 @@ async function run() {
       res.send(paymentResult);
     })
 
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
